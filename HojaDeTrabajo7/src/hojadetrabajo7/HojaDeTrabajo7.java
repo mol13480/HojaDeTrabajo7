6aa40dd91186167d10306e7126a8bf0e96dec3b0
Irene <Irene@192.168.1.6>
2014-09-09 15:29:32 -0600

    Modificación

--- HojaDeTrabajo7/src/hojadetrabajo7/HojaDeTrabajo7.java
+++ HojaDeTrabajo7/src/hojadetrabajo7/HojaDeTrabajo7.java
@@ -1,22 +1,78 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+Universidad del Valle de Guatemala
+Hoja de Trabajo 7 
+9/09/2014
+Irene Molina 13480
+Jorge Garcia 13175
+Moises Urias 13015
  */
 
 package hojadetrabajo7;
 
-/**
- *
- * @author Irene
- */
-public class HojaDeTrabajo7 {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.*;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class HojaDeTrabajo7 {
     
+    
+    private static List<HashMap<String,String>> array = new ArrayList<HashMap<String,String>>();
+    
+    
+    public static void main(String[] args) {
+        leerArchivo(array);
+    }
+    //metodo para leer el archivo de texto 
+    public static void leerArchivo(List<HashMap<String,String>> imp){
+    
+        
+       
+         /*Declaracion de variables*/
+        String micadena = null;
+        String linea=null;
+        
+        
+        FileReader fr;
+        BufferedReader br;
+                
+        
+        try
+        {
+            /*Se crea un objeto que permite abrir el archivo de texto*/
+            AbrirJFileChooser abrirArchivo = new AbrirJFileChooser();
+            abrirArchivo.Abrir(); /*Se slecciona el archivo de texto*/
+            String ruta =abrirArchivo.getPath(); /*Se obtiene la ruta del
+                                                    archivo de texto*/
+           
+            
+            /*Lectura del archivo*/
+            fr = new FileReader(ruta);
+            br = new BufferedReader(fr);
+            
+            while ((micadena = br.readLine())!=null)
+            {
+                linea = micadena;
+               
+                String[] tokens = micadena.split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)");
+                for(String t : tokens) {
+              
+                    System.out.println("> "+t);
+                
+                 }
+          
+            }
+        }
+        
+        /*Error en caso de que no se pueda leer el archivo*/
+        catch(Exception e)
+        {
+            System.out.println("Hubo un error leyendo el archivo. No se"
+                    + "encuentra el archivo 'datos.txt'");
+            
+            
+             /*Si hubo error cargando el archivo, termina el programa*/
+            System.exit(0);
+        }
+    
+    }
 }
