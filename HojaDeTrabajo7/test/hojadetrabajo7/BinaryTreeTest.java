897f0ab679a53648ae66371e2cc253aeb1b5a7f8
Irene <Irene@Irene-PC>
2014-09-12 21:16:33 -0600

    Modificación

---
+++ HojaDeTrabajo7/test/hojadetrabajo7/BinaryTreeTest.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package hojadetrabajo7;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Irene
+ */
+public class BinaryTreeTest {
+    
+    public BinaryTreeTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of addNode method, of class BinaryTree.
+     */
+    @Test
+    public void testAddNode() {
+        System.out.println("addNode");
+        BinaryTree instance = new BinaryTree();
+        instance.addNode("1", "Primero");
+        System.out.println("Se agregó: (1, Primero)");
+        Node resultado = instance.findNode("1");
+        System.out.println("Se encontro:"+resultado); 
+    }
+
+    
+
+   
+
+    /**
+     * Test of findNode method, of class BinaryTree.
+     */
+    @Test
+    public void testFindNode() {
+        System.out.println("findNode");
+        BinaryTree instance = new BinaryTree();
+        instance.addNode("1", "Primero");
+        System.out.println("Se agregó: (1, Primero)");
+        Node resultado = instance.findNode("1");
+        System.out.println("Se busco:1, encontro:"+resultado); 
+    }
+
+    
+}
